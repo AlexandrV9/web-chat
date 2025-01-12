d3281859605e3b195747dda4175f7c1fbe0b2fc2
Alexandr <vashchekin01@mail.ru>
2025-01-12 16:09:45 +0300

    refac: add import other pages in vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,18 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, "./dist"),
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"),
+
+        home: resolve(__dirname, "./src/pages/home/index.html"),
+        signIn: resolve(__dirname, "./src/pages/sign-in/index.html"),
+        signUp: resolve(__dirname, "./src/pages/sign-up/index.html"),
+        profile: resolve(__dirname, "./src/pages/profile/index.html"),
+        error500: resolve(__dirname, "./src/pages/500/index.html"),
+        error404: resolve(__dirname, "./src/pages/404/index.html"),
+      },
+    },
   },
   css: {
     modules: {
